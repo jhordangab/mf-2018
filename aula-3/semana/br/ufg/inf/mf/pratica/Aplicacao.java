d8ef2c821ab8bdd8a6032015a0a7b9d0364cfc82
Jhordan <jhordangabriel@hotmail.com>
2018-09-23 21:23:36 -0300

    working with javac

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,10 @@
+package br.ufg.inf.mf.pratica;
+
+import java.time.LocalDate;
+
+class Aplicacao {
+  public static void main(String[] args) {
+    System.out.println(LocalDate.now().getDayOfWeek());
+  }
+}
+
