dfefff29c5bb77047e52b5683d52066a1fb77903
Jhordan Gabriel <jhordangabriel@hotmail.com>
2018-10-01 20:47:06 -0300

    aula 5 com maven

---
+++ aula-5/cnes/src/main/java/br/ufg/inf/mf/pratica/cnes/App.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.mf.pratica.cnes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
